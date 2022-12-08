955d897f8dc7df66958f1e3cb9067f5a7cea44eb
francipe <franciellenocp@gmail.com>
2022-12-08 19:43:26 -0300

    Adicionando novas classes

--- src/tech/ada/banco/Pessoa.java
+++ src/tech/ada/banco/Pessoa.java
@@ -1,32 +1,79 @@
 package tech.ada.banco;
 
-public class Pessoa {
-    String nome;
-    String dataNascimento; //Local Date
-    String cpf;
-    String telefone;
+import tech.ada.banco.exception.IdadeInvalidaException;
 
-    public Pessoa(String nome, String cpf, String dataNascimento) {
+public class Pessoa {
+    private String nome;
+    private String sobreNome;
+    private int idade;
+
+//    public Pessoa(String nome, String cpf, String dataNascimento) {
+////        System.out.println(cpf);       // da função - parâmetro do método
+////        System.out.println(this.cpf); // acessar o objeto - atributo do construtor
+//        this.cpf = cpf; //
+//        this.dataNascimento = dataNascimento;
+//        this.nome = nome;
+//
+//    }
+
+//    public Pessoa(String nome, String cpf, String dataNascimento, String telefone) {
 //        System.out.println(cpf);       // da função - parâmetro do método
 //        System.out.println(this.cpf); // acessar o objeto - atributo do construtor
-        this.cpf = cpf; //
-        this.dataNascimento = dataNascimento;
-        this.nome = nome;
+//        this.cpf = cpf; //
+//        this.dataNascimento = dataNascimento;
+//        this.nome = nome;
+//        this.telefone = telefone;
+//
+//    }
 
+    public Pessoa(String nome, String sobreNome, int idade) {
+        this.nome = nome;
+        this.sobreNome = sobreNome;
+        setIdade(idade);
     }
 
-    public Pessoa(String nome, String cpf, String dataNascimento, String telefone) {
-        System.out.println(cpf);       // da função - parâmetro do método
-        System.out.println(this.cpf); // acessar o objeto - atributo do construtor
-        this.cpf = cpf; //
-        this.dataNascimento = dataNascimento;
+    public Pessoa(String nome, String sobreNome) {
         this.nome = nome;
-        this.telefone = telefone;
-
+        this.sobreNome = sobreNome;
     }
 
     @Override
     public String toString() {
-        return "Nome: " + nome + " Data de Nascimento:" + dataNascimento + " Telefone: " + telefone;
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", sobreNome='" + sobreNome + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        if (idade < 0) {
+            throw new IdadeInvalidaException("Idade não pode ser negativa.");
+        } else if (idade > 150) {
+            throw new IdadeInvalidaException("Idade não pode ser maior que 150.");
+        } else {
+            this.idade = idade;
+        }
     }
 }
+
---
+++ src/tech/ada/banco/exception/IdadeInvalidaException.java
@@ -0,0 +1,9 @@
+package tech.ada.banco.exception;
+
+public class IdadeInvalidaException extends RuntimeException {
+
+    public IdadeInvalidaException(String errorMessage) {
+        super(errorMessage);
+    }
+}
+
